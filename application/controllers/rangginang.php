b38b6db637e83f4c3df996f430711ba888d296a5
Reza Fahneri <rezafahneri@gmail.com>
2021-05-18 14:30:22 +0700

    update dikit

--- application/controllers/rangginang.php
+++ application/controllers/rangginang.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Rangginang extends CI_Controller {
 	public function index()
 	{
-		$data['title']="UMKM Toko Rangginang Ceu Tuti| Bogorfood";
+		$data['title']="UMKM Rangginang Ceu Tuti| Bogorfood";
 		$data['rangginang_tuti']= $this->Model_toko_rangginang->get_data('tbl_rangginang_tuti')->result();
 		$this->load->view('template_customer/header',$data);
         $this->load->view('v_rangginang');
--- application/controllers/tobo.php
+++ application/controllers/tobo.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Tobo extends CI_Controller {
 	public function index()
 	{
-		$data['title']="UMKM Tobokito| Bogorfood";
+		$data['title']="UMKM Tobo Kito| Bogorfood";
 		$data['tobo']= $this->Model_toko_tobo->get_data('tbl_tobokito')->result();
 		$this->load->view('template_customer/header',$data);
         $this->load->view('v_tobo');
